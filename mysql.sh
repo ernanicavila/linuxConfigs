c77258d8e792edacd1de011a7d4064d8252cf924
Ernani Ávila <ernanicavilaneto@gmail.com>
2022-12-14 11:05:03 -0300

    corrected mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo snap install mysql-workbench-community
+sudo apt-get install mysql-workbench-community
 
 #Remove Workbench Community, uncomment the following line:
 #snap remove mysql-workbench-community
