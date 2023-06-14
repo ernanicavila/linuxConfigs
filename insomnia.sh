39f039a5a8809e3a2efef45e13481567376af556
Ernani Ávila <ernanicavilaneto@gmail.com>
2023-06-14 14:17:08 -0300

    changed insomnia.sh

--- insomnia.sh
+++ insomnia.sh
@@ -4,5 +4,4 @@ echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ 
   sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
 # Refresh repository sources and install Insomnia
-sudo apt-get update
 sudo apt-get install insomnia
